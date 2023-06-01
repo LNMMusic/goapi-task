62376a36eebc2dfe6df2a9e421e6068f56023472
Lucas Masiero <lucas.masi.ero@hotmail.com>
2023-06-01 20:16:00 -0300

    feat cmd: added new handler for health check

---
+++ cmd/rest/handlers/impl_handler_health.go
@@ -0,0 +1,10 @@
+package handlers
+
+import "net/http"
+
+func Health() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("pong"))
+	}
+}
